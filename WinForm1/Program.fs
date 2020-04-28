d402eb14a34544ebfc59d7ed80a4f4cff9db97b5
Roberto Kai Hsi Hoo <prof.robertohoo@gmail.com>
2020-04-28 12:58:49 -0300

    ok

--- WinForm1/Program.fs
+++ WinForm1/Program.fs
@@ -27,10 +27,92 @@ let randomBrush() =
     let r, g, b = rnd.Next(256), rnd.Next(256), rnd.Next(256)
     new SolidBrush(Color.FromArgb(r, g, b))
 
+// Listing 4.8: Drawing a segment of the pie chart
+// Draws a part of the pie chart (specified by startAngle 
+// & occupiedAngle) on the graphics 'gr' (label is ignored)
+let drawPieSegment (gr : Graphics, title, startAngle, occupiedAngle) = 
+    let br = randomBrush()
+    gr.FillPie(br, 170, 70, 260, 260, startAngle, occupiedAngle)
+    br.Dispose()
+
+// Listing 4.11: Drawing text labels
+let fnt = new Font("Times New Roman", 11.0f)
+let centerX, centerY = 300.0, 200.0
+let labelDistance = 150.0
+
+// Draws a label for a part of pie graph 
+// The signature is same as a signature of 'drawPie'
+let drawLabel (gr : Graphics, title, startAngle, occupiedAngle) = 
+    // Calculate angle, where the text should be (in radians)
+    let ra = (Math.PI * 2.0 * float ((startAngle + occupiedAngle / 2)) / 360.0)
+    // ... and position for the center of the text
+    let x = centerX + labelDistance * cos (ra)
+    let y = centerY + labelDistance * sin (ra)
+    // Get rectangle, in which the text will be drawn & draw it
+    let size = gr.MeasureString(title, fnt)
+    let rc = new PointF(float32 (x) - size.Width / 2.0f, float32 (y) - size.Height / 2.0f)
+    gr.DrawString(title, fnt, Brushes.Black, new RectangleF(rc, size))
+
+// Listing 4.9: Drawing items using specified drawing function
+let drawStep (drawingFunc, gr : Graphics, sum, data) = 
+    // Nested recursive function that processes the data
+    let rec drawStepUtil (data, angleSoFar) = 
+        match data with
+        // Matches an empty list 
+        | [] -> ()
+        // Matches a list with a single element
+        | [ title, value ] -> 
+            // Calculate the angle to add up to 360 
+            let angle = 360 - angleSoFar
+            // Draw the segment
+            drawingFunc (gr, title, angleSoFar, angle)
+        // Matches a list with non-empty tail
+        | (title, value) :: tail -> 
+            let angle = int (float (value) / sum * 360.0)
+            // Draw the segment
+            drawingFunc (gr, title, angleSoFar, angle)
+            // Recursively draw the rest
+            drawStepUtil (tail, angleSoFar + angle)
+    // Run the local utility function
+    drawStepUtil (data, 0)
+
+// Listing 4.10: Drawing the chart
+// Load data from the specified file, process the data and draw the chart
+let drawChart (file) = 
+    // Load file, convert it to list of tuples and calculate summary
+    let lines = List.ofSeq (File.ReadAllLines(file))
+    let data = processLines lines
+    let sum = float (calculateSum (data))
+    // Create a bitmap and object for drawing on it
+    let pieChart = new Bitmap(600, 400)
+    let gr = Graphics.FromImage(pieChart)
+    // Fill with white color
+    // Draw chart using two phases - first the chart then the labels
+    gr.Clear(Color.White)
+    drawStep (drawPieSegment, gr, sum, data)
+    drawStep (drawLabel, gr, sum, data)
+    // Finalize drawing & return bitmap
+    gr.Dispose()
+    pieChart
+
+// Listing 4.12: Adding user interaction
+// 'Open' button handler - show dialog, draw the image and display it
+let openAndDrawChart (e) = 
+    let dlg = new OpenFileDialog(Filter = "CSV Files|*.csv")
+    if (dlg.ShowDialog() = DialogResult.OK) then 
+        let pieChart = drawChart dlg.FileName
+        boxChart.Image <- pieChart
+        btnSave.Enabled <- true
+
+// 'Save' button handler - show dialog & save currently generated image    
+let saveDrawing (e) = 
+    let dlg = new SaveFileDialog(Filter = "PNG Files|*.png")
+    if (dlg.ShowDialog() = DialogResult.OK) then boxChart.Image.Save(dlg.FileName)
+
 // Entry-point for the graphical version   
 [<STAThread>]
 do // Specify event handlers - a function called when button is clicked
-   //btnOpen.Click.Add(openAndDrawChart)
-   //btnSave.Click.Add(saveDrawing)
+   btnOpen.Click.Add(openAndDrawChart)
+   btnSave.Click.Add(saveDrawing)
    Application.EnableVisualStyles()
    Application.Run(mainForm)
